20c080fddd92063035e1db7f8523a9b4fbaa4708
AllaVoitiuk <allavoytjuk@ukr.net>
2022-11-11 23:04:52 +0200

    markap

--- src/index.js
+++ src/index.js
@@ -15,3 +15,29 @@ axios.get('/users')
       console.error(error);
     }
   }
+
+  //let country = 'germany'
+// function test(){
+//     fetch(`https://restcountries.com/v3.1/name/${country}`).then(response => response.json()).then(data => console.log(data))
+// }
+// test()
+//
+// function getData(){
+//     fetch('https://rickandmortyapi.com/api/character/?count=30')
+//         .then(response => response.json())
+//         .then(data => {
+//             const {results: dataArray} = data
+//             const markup = dataArray.map(item => `
+//     <li>
+//         <img src=${item.image} alt="">
+//         <p>NAME: ${item.name}</p>
+//         <p>GENDER: ${item.gender}</p>
+//         <p>ID: ${item.id}</p>
+//         <p>STATUS: ${item.status}</p>
+//         <p>CREATED: ${item.created}</p>
+//     </li>
+// `).join('')
+//             document.querySelector('.characterList').innerHTML += markup
+//         })
+// }
+// getData()

8563dbbc9a263db4c3a14591622f27d80eaa4bff
AllaVoitiuk <allavoytjuk@ukr.net>
2022-11-23 02:07:49 +0200

    hw11

--- src/index.js
+++ src/index.js
@@ -1,43 +1,149 @@
 import Notiflix from 'notiflix';
 import axios from "axios";
-axios.get('/users')
-  .then(res => {
-    console.log(res.data);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+let lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
+
+const API_KEY = '31475177-5e18f0fae26a0bf9f0a41710d';
+
+let refs = {
+  form: document.querySelector("#search-form"),
+  input: document.getElementsByName("searchQuery"),
+  submitBtn: document.querySelector('button'), 
+  loadBtn: document.querySelector('.load-more')
+}
+
+refs.submitBtn.addEventListener("click", onSubmit);
+//console.log("hello");
+let counter = 1;
+let inputValue = "";
+let hitsCount = 0;
+let totalHits = 0;
+
+function getData(picName) {
+  let URL = "https://pixabay.com/api/?key=" + API_KEY +
+  "&q=" + encodeURIComponent(picName) + 
+  `&image_type=photo&orientation=horizontal&safesearch=true&page=${counter}&per_page=40`;
+
+  return axios.get(URL);
+ 
+  lightbox.on('show.simplelightbox', function () {
+   
   });
 
+}
+
+ function onSubmit(event) {
+  event.preventDefault();
+
+  totalHits = 0;
+  hitsCount = 0;
+  inputValue = refs.input[0].value;
+  
+  console.log("inputValue: " + inputValue);
+  inputValue = inputValue.trim();
+  
+  document.querySelector('.gallery').innerHTML = "";
+  counter = 1;  
+  
+  getGallery(inputValue);   
+}
+
+const galleryContainer = document.querySelector(".photo-card");
+
+async function getGallery(inputValue){
+
+  refs.loadBtn.classList.remove("load-active");
+
+  console.log("getGalery(), counter = " + counter);
+  let markupDiv = "";
+  
+  if(inputValue){
+
+    await getData(inputValue)
+    .then(response => {
+
+      if (response.data.hits.length === 0) {
+          
+        Notiflix.Notify.failure(`Sorry, there are no images matching your search query. Please try again.`);
+      }
+      else{
+        
+        console.log(response.data);        
+        
+        response.data.hits.forEach(element => {
+          
+          markupDiv += `<a class="gallery__item" href=${element.largeImageURL}>
+          <div class="photo-card">          
+          <img src=" ${element.webformatURL}" alt="" loading="lazy" />
+          <div class="info">
+          <p class="info-item">
+            <b>Likes</b> ${element.likes}
+          </p>
+          <p class="info-item"> 
+            <b>Views</b> ${element.views}
+          </p>
+          <p class="info-item">
+            <b>Comments</b> ${element.comments}
+          </p>
+          <p class="info-item">
+            <b>Downloads</b> ${element.downloads}
+          </p>
+          </div>
+          </div>
+          </a>`;
+                          
+          //console.log(element.webformatURL);
+        });      
+        document.querySelector('.gallery').innerHTML += markupDiv;
+        counter+=1;
+
+        totalHits = response.data.totalHits;
+        if(hitsCount == 0){
+          Notiflix.Notify.success(`Hooray! We found ` + totalHits + ` images.`);
+        }
+        
+        hitsCount += 40;
+        
+        console.log("hitsCount: " + hitsCount);             
+        console.log("totalHits: " + totalHits);         
+
+        if(hitsCount <= totalHits){
+
+          console.log("Show button");
+          refs.loadBtn.classList.add("load-active");
+        } else {
+          Notiflix.Notify.info(`We're sorry, but you've reached the end of search results.`);
+          console.log(`We're sorry, but you've reached the end of search results.`)
+        }
+      }
+    })    
+  }  
+  lightbox.refresh();
+}
+
+function changeGallery(event){
+  getGallery(inputValue);
  
+}
 
-  async function getUser() {
-    try {
-      const response = await axios.get('/user?ID=12345');
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
-  }
+// window.addEventListener('scroll', () => {
+//   const {scrollHeight, scrollTop, clientHeight} = document.documentElement
+//   if(scrollHeight - clientHeight === scrollTop){
+//     getData();
+//   }
+// })
 
-  //let country = 'germany'
-// function test(){
-//     fetch(`https://restcountries.com/v3.1/name/${country}`).then(response => response.json()).then(data => console.log(data))
-// }
-// test()
-//
-// function getData(){
-//     fetch('https://rickandmortyapi.com/api/character/?count=30')
-//         .then(response => response.json())
-//         .then(data => {
-//             const {results: dataArray} = data
-//             const markup = dataArray.map(item => `
-//     <li>
-//         <img src=${item.image} alt="">
-//         <p>NAME: ${item.name}</p>
-//         <p>GENDER: ${item.gender}</p>
-//         <p>ID: ${item.id}</p>
-//         <p>STATUS: ${item.status}</p>
-//         <p>CREATED: ${item.created}</p>
-//     </li>
-// `).join('')
-//             document.querySelector('.characterList').innerHTML += markup
-//         })
-// }
-// getData()
+refs.loadBtn.addEventListener('click', changeGallery);
+
+const { height: cardHeight } = document
+  .querySelector(".gallery")
+  .firstElementChild.getBoundingClientRect();
+
+window.scrollBy({
+  top: cardHeight * 2,
+  behavior: "smooth",
+});
+
+
